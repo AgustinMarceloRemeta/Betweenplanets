c01a4d966c5d5381a340ec8c5b7549aa680883b0
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-08-01 17:32:48 -0300

    Arreglos

--- Assets/Scrips/EndManager.cs
+++ Assets/Scrips/EndManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndManager : MonoBehaviour
 {
-    [SerializeField] float time;
+    [SerializeField] float time, Moneys;
+    [SerializeField] Text texto;
     void Update()
     {
+        if (PlayerPrefs.GetInt("Money", 0) == Moneys)
+            texto.text = "Felicidades conseguiste todas las monedas";
+        else texto.text = "Te faltaron " + (Moneys - PlayerPrefs.GetInt("Money", 0)).ToString() + " monedas";
         time -= Time.deltaTime;
         if (time <= 0) SceneManager.LoadScene(0);
     }
